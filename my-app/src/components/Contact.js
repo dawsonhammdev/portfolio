6b7565e4ff3393c960fc2d4a448be170f76065dd
dawsonhammdev <hammsdesigns@gmail.com>
2020-06-16 12:09:03 -0500

    responsiveness to larger screens

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import data from '../yourdata';
+import resume from '../img/resume.pdf'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
 
 class Contact extends Component {
     state = {  }
     render() { 
         return (  <div>
         <h1>
-                <Fade bottom cascade> Contact.</Fade>
+                <Fade bottom cascade>Contact.</Fade>
         </h1>
         <Fade bottom>
         <div className='contact-content'>
                 <h1>
                 Let’s create your next<br></br>
                 <span className='amazing-color'>experience together</span></h1>
+                <button>
+                    <a href={resume} download>
+                            Resume
+                            <FontAwesomeIcon icon={faDownload} className='icon' />
+                    </a>
+                </button><br></br>
                     <a href={`mailto:${data.contactEmail}`} className='email'>{data.contactEmail}</a>
                 <ul>
                     {data.social.map((link,index)=>(
                         <li key={index}><a target='_blank' rel="noopener noreferrer" href={link.url}>{link.name}</a></li>
                     ))}   
                 </ul>
+                
         </div>
         </Fade>
 
